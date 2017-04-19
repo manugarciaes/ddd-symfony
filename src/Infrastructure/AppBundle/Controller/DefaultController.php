6685f5ce32e0a9dd019d22c895491692591e1d06
Manu Garcia <manugarciaes@gmail.com>
2017-04-19 16:04:39 +0200

    comments

--- src/Infrastructure/AppBundle/Controller/DefaultController.php
+++ src/Infrastructure/AppBundle/Controller/DefaultController.php
@@ -4,8 +4,15 @@ namespace Infrastructure\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class DefaultController
+ * @package Infrastructure\AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('InfrastructureAppBundle:Default:index.html.twig');
--- src/Infrastructure/AppBundle/InfrastructureAppBundle.php
+++ src/Infrastructure/AppBundle/InfrastructureAppBundle.php
@@ -4,6 +4,10 @@ namespace Infrastructure\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class InfrastructureAppBundle
+ * @package Infrastructure\AppBundle
+ */
 class InfrastructureAppBundle extends Bundle
 {
 }
