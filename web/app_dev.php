97ab2b2e7fe945fccd08b8d55f3f8b3d9a3cd20a
Manu Garcia <manugarciaes@gmail.com>
2017-04-19 15:40:31 +0200

    fix app_dev with docker

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,10 +12,12 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '192.168.33.1', 'fe80::1', '::1', '192.168.99.1']) || php_sapi_name() === 'cli-server')
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    if (0 === preg_match('/^172\.\d{1,3}\.0\.1$/', @$_SERVER['REMOTE_ADDR'])) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    }
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
